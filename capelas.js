8fc2c385a950a7e29e581d95f24b3601d6399472
Faladerix <100221487+gderich@users.noreply.github.com>
2026-09-20 15:35:17 -0300

    Adiciona capelas ao mapa e aos cartazes

--- capelas.js
+++ capelas.js
@@ -10,4 +10,20 @@
      { nome: 'Capela Messejana', lat: -3.8365, lng: -38.4905 },
    ];
    ============================================================ */
-const CAPELAS = [];
+const CAPELAS = [
+  {
+    nome: 'Capela Messejana',
+    lat: -3.8218319,
+    lng: -38.4958741,
+  },
+  {
+    nome: 'Capela Francisco Costa',
+    lat: -3.84081031,
+    lng: -38.51631124,
+  },
+  {
+    nome: 'Capela Valparaíso',
+    lat: -3.8484077,
+    lng: -38.511885,
+  },
+];
